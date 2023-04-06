7702bf42c5990a9a3c6d3a7bea6dc0ad443ea9db
insanz01 <insankamil002@gmail.com>
2023-04-06 15:25:02 +0700

    fix upload file

--- application/controllers/AppController.php
+++ application/controllers/AppController.php
@@ -38,14 +38,14 @@ class AppController extends CI_Controller
 
   public function do_unggah() {
     $config['upload_path']          = './uploads/';
-    $config['allowed_types']        = 'gif|jpg|png';
+    $config['allowed_types']        = 'xlsx|csv';
     $config['max_size']             = 100;
     $config['max_width']            = 1024;
     $config['max_height']           = 768;
 
     $this->load->library('upload', $config);
 
-    if ( ! $this->upload->do_upload('userfile'))
+    if ( ! $this->upload->do_upload('dokumen'))
     {
       $error = array('error' => $this->upload->display_errors());
 
